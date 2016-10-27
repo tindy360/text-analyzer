5637ffc0ef88a48825ef3e8afe543732b2a97cc2
Tom Indellicate <Tom@Toms-MacBook-Pro.local>
2016-10-27 06:17:10 -0400

    fully buildt but reload bug

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
 //when user submits form listen for event.
 $('form').submit(function(event) {
-    // prevent page form re loading
-    event.preventDefault();
-    // declare variables for data being worked with and set to 0
-    var avgWordLength, wordTotalLength = 0;
-    var uniqueWords, wordCount = 0;
-    var countSentences = 0;
-    //
-    //input/store the user submited data in to the variables
-    //split puncuation and spaces for word count
-    countSentences = ($('user-text').val().split(. ? !))
-    wordCount = ($('user-text').val().split(' '));
-    uniquewords, ($('user-text').val().split(' '));
-
-    // count through words
-
-    // count through unique words elminate duplicates return lenght
-
-
-    //count through puncuation to find number of sentences and divide for avg length
-
-    // DOM  remove class for hidden classes
-
-    //assign the output data to previously hidden clases 
 
 
 
+            // declare variables for data being worked with and set to 0
+            var avgWordLength = 0;
+            var uniqueWords = 0;
+            var wordCount, wordReturn = 0;
+            var countSentences, sentencesReturn =0;
+            var avgSentenceLength = 0;
+
+            event.preventDefault();
+            //
+            //input/store the user submited data in to the variables
+            //split puncuation and spaces for word count
+            countSentences = ($('user-text').val().split((. ? !)); wordCount = ($('user-text').val().split((' ')); uniqueWords = ($('user-text').val().split((' '));
+
+                    // count through words
+                    for (var i = 0; i < wordCount.length; i++) {
+                      wordReturn += wordCount[i].length;
+                    }
+
+                    // count through sentences
+                    for (var i = 0; i< countSentences.length; i++) {
+                      sentencesReturn += countSentences[i].split(' ').length;
+                    }
 
 
-});
+                    // count through unique words eliminate duplicates return lenght
+                    uniqueWords = jQuery.uniqueSort(uniqueWords);
+
+                    //count through puncuation to find number of sentences and divide for avg length
+                    avgSentenceLength = (countSentences.length / wordCount.length);
+
+                    //run math for average word lenght
+                    avgWordLength = (wordReturn / worCount.length);
+
+
+                    // DOM  remove class for hidden classes
+                    $('.text-report').removeClass(hidden);
+                    //assign the output data to previously hidden clases
+                    $('word-count').text(wordsCounted);
+                    $('unique-words').text(uniqueWords);
+                    $('average-word-length').text(avgWordLength);
+                    $('average-sentence-length').text(avgSentenceLength)
+
+                });
+
+
+
+            });
